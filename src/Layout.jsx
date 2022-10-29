f29d52de7ada920c007054a00a0e71a761cdad86
CarlosEncisoUdeCaldas <carlosenciso.tic@ucaldas.edu.co>
2022-10-29 15:08:53 -0500

    Props Type ok

---
+++ src/Layout.jsx
@@ -0,0 +1,23 @@
+import "./Layout.css"
+import Saludo from "./Saludo/Saludo";
+
+
+//nuestro primer componente creado en React
+const Layout = ( ) => {
+    const grupo = 32 ;
+    const nombre =  'Carlitos';
+    const h2Style = { color: 'green', backgroundColor: 'white', padding:'20px' } //estilo para la etiqueta h2
+
+    return (
+        <>
+            <h1 className = "saludo" > Hola Mundo Grupo { grupo }</h1>
+            {/* <h2 style={ h2Style } > Bievenido { nombre } </h2>
+            <p>Esto es un parrafo</p> */}
+            {/* importacion del componente Saludo */}
+            <Saludo />
+        </>
+        
+    );
+}
+
+export default Layout;
---
+++ src/Saludo/Saludo.jsx
@@ -0,0 +1,26 @@
+import PropTypes from 'prop-types'
+
+const Saludo = ( { nombre, apellido } ) => {
+    // const { nombre, apellido } = props; //desestructuracion del objeto props
+
+  return (
+    <>
+        <p> Saludo Sr(a). { nombre } { apellido }  </p>
+    </>
+  )
+}
+
+export default Saludo
+
+//aqui voy a definir el tipo de dato para las props a través de PropTypes
+Saludo.propTypes = {
+    nombre: PropTypes.string.isRequired,
+    apellido: PropTypes.string
+}
+
+//tambien tenemos la opcion de crear valores por defecto para las props
+//defaultProps
+Saludo.defaultProps = {
+    
+    apellido: '@sinApellido'
+}
